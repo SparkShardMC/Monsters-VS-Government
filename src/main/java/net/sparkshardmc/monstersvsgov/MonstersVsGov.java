dca872a1600da8af51e61b23391a2f0308ed161a
ScriptBox <zaki.coding0119@gmail.com>
2026-04-12 04:05:03 -0700

    Update MonstersVsGov.java

--- src/main/java/net/sparkshardmc/monstersvsgov/MonstersVsGov.java
+++ src/main/java/net/sparkshardmc/monstersvsgov/MonstersVsGov.java
@@ -1,9 +1,12 @@
 package net.sparkshardmc.monstersvsgov;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +14,7 @@ public class MonstersVsGov implements ModInitializer {
 
     public static final String MOD_ID = "monstersvsgov";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final ResourceLocation FACTION_PACKET_ID = new ResourceLocation(MOD_ID, "faction_select");
 
     @Override
     public void onInitialize() {
@@ -24,6 +28,14 @@ public class MonstersVsGov implements ModInitializer {
                 data.putInt("FactionID", 0);
                 LOGGER.info("§e[!] Initializing new identity for: " + player.getName().getString());
             }
+        });
+
+        ServerPlayNetworking.registerGlobalReceiver(FACTION_PACKET_ID, (server, player, handler, buf, responseSender) -> {
+            int factionId = buf.readInt();
+            server.execute(() -> {
+                player.getPersistentData().putInt("FactionID", factionId);
+                LOGGER.info("§fPlayer " + player.getName().getString() + " confirmed as faction: " + factionId);
+            });
         });
     }
 }
