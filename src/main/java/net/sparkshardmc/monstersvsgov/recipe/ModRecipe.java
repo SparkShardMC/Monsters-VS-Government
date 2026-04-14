8546557a38c652717ac0bc34a1f5956ebf5b9b1e
ScriptBox <zaki.coding0119@gmail.com>
2026-04-14 15:48:21 -0700

    Update ModRecipe.java

--- src/main/java/net/sparkshardmc/monstersvsgov/recipe/ModRecipe.java
+++ src/main/java/net/sparkshardmc/monstersvsgov/recipe/ModRecipe.java
@@ -2,16 +2,17 @@ package net.sparkshardmc.monstersvsgov.recipe;
 
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.crafting.RecipeSerializer;
-import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.Registry;
 import net.sparkshardmc.monstersvsgov.MonstersVsGov;
+import java.util.function.Supplier;
 
 public class ModRecipes {
-    public static void register( ) {
-        // This connects your Java logic to the JSON files
-        Registry.register(BuiltInRegistries.RECIPE_SERIALIZER, 
+    public static Supplier<RecipeSerializer<DaggerRepairRecipe>> DAGGER_REPAIR_SERIALIZER;
+
+    public static void register() {
+        DAGGER_REPAIR_SERIALIZER = () -> Registry.register(BuiltInRegistries.RECIPE_SERIALIZER, 
             new ResourceLocation(MonstersVsGov.MOD_ID, "dagger_repair"), 
-            new ShapedRecipe.Serializer());
+            new ShapedRecipe.Serializer()); // We use the Shaped Serializer as a base
     }
 }
