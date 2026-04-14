eaf00789d3edfb0f15d0133362015fda052662ba
ScriptBox <zaki.coding0119@gmail.com>
2026-04-14 15:53:18 -0700

    Update and rename ModRecipe.java to ModRecipes.java

---
+++ src/main/java/net/sparkshardmc/monstersvsgov/recipe/ModRecipes.java
@@ -0,0 +1,18 @@
+package net.sparkshardmc.monstersvsgov.recipe;
+
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.sparkshardmc.monstersvsgov.MonstersVsGov;
+
+public class ModRecipes {
+    // Standard serializer field
+    public static final RecipeSerializer<DaggerRepairRecipe> DAGGER_REPAIR_SERIALIZER = new DaggerRepairRecipe.Serializer();
+
+    public static void register() {
+        Registry.register(BuiltInRegistries.RECIPE_SERIALIZER, 
+            new ResourceLocation(MonstersVsGov.MOD_ID, "dagger_repair"), 
+            DAGGER_REPAIR_SERIALIZER);
+    }
+}
