3b58467cc2005f3b4d053f42870a2be5323074a7
ScriptBox <zaki.coding0119@gmail.com>
2026-04-14 15:48:06 -0700

    Update DaggerRepairRecipe.java

--- src/main/java/net/sparkshardmc/monstersvsgov/recipe/DaggerRepairRecipe.java
+++ src/main/java/net/sparkshardmc/monstersvsgov/recipe/DaggerRepairRecipe.java
@@ -1,10 +1,9 @@
 package net.sparkshardmc.monstersvsgov.recipe;
 
-import net.minecraft.core.NonNullList;
 import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.ShapedRecipe;
 import net.minecraft.world.level.Level;
 import net.sparkshardmc.monstersvsgov.item.ModItems;
@@ -16,12 +15,17 @@ public class DaggerRepairRecipe extends ShapedRecipe {
 
     @Override
     public boolean matches(CraftingContainer container, Level level) {
-        // First check if the items are in the right spots (The JSON pattern)
+        // First check if the items are in the right pattern spots
         if (super.matches(container, level)) {
-            // Check slot 8 (Bottom-Right in 3x3) for exactly 3 Gold
+            // Check slot 8 (Bottom-Right) for exactly 3 Gold
             ItemStack goldSlot = container.getItem(8);
             return goldSlot.is(Items.GOLD_INGOT) && goldSlot.getCount() == 3;
         }
         return false;
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return ModRecipes.DAGGER_REPAIR_SERIALIZER.get();
     }
 }
