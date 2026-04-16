77b1be0374ed09e76dd3bb2ba1fe44d087a9d7de
ScriptBox <zaki.coding0119@gmail.com>
2026-04-16 16:27:29 -0700

    Update MonstersVsGovClient.java

--- src/main/java/net/sparkshardmc/monstersvsgov/client/MonstersVsGovClient.java
+++ src/main/java/net/sparkshardmc/monstersvsgov/client/MonstersVsGovClient.java
@@ -1,24 +1,20 @@
 package net.sparkshardmc.monstersvsgov.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.Minecraft;
+import net.sparkshardmc.monstersvsgov.MonstersVsGov;
 import net.sparkshardmc.monstersvsgov.client.screen.EntryScreen;
 
 public class MonstersVsGovClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        // Logic: When the client joins a server, check if they need to see the Entry Screen
-        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
-            // We use a small delay or a check to see if the player has a faction
-            // In 26.1.2, the easiest way is to check the local player's data
+        ClientPlayNetworking.registerGlobalReceiver(MonstersVsGov.FACTION_SYNC_ID, (client, handler, buf, responseSender) -> {
+            int factionId = buf.readInt();
             client.execute(() -> {
-                if (client.player != null) {
-                    // This opens the GUI if they are currently Faction 0 (Neutral)
-                    // Note: PersistentData is server-side, so usually you'd trigger this via a packet
-                    // For now, this is where you'd put the screen trigger:
-                    // Minecraft.getInstance().setScreen(new EntryScreen());
+                if (factionId == 0) {
+                    Minecraft.getInstance().setScreen(new EntryScreen());
                 }
             });
         });
