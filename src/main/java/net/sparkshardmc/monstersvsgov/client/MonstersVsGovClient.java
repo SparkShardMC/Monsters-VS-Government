1b6277854207d161911851a8bb542d6d9f5d9782
ScriptBox <zaki.coding0119@gmail.com>
2026-04-16 16:12:59 -0700

    Update MonstersVsGovClient.java

--- src/main/java/net/sparkshardmc/monstersvsgov/client/MonstersVsGovClient.java
+++ src/main/java/net/sparkshardmc/monstersvsgov/client/MonstersVsGovClient.java
@@ -1,57 +1,26 @@
-package net.sparkshardmc.monstersvsgov;
+package net.sparkshardmc.monstersvsgov.client;
 
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.resources.ResourceLocation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.minecraft.client.Minecraft;
+import net.sparkshardmc.monstersvsgov.client.screen.EntryScreen;
 
-public class MonstersVsGov implements ModInitializer {
-
-    public static final String MOD_ID = "monstersvsgov";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    public static final ResourceLocation FACTION_PACKET_ID = new ResourceLocation(MOD_ID, "faction_select");
+public class MonstersVsGovClient implements ClientModInitializer {
 
     @Override
-    public void onInitialize() {
-        LOGGER.info("§8[SYSTEM] net.sparkshardmc.java core loading...");
-
-        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
-            ServerPlayer player = handler.getPlayer();
-            CompoundTag data = player.getPersistentData();
-
-            if (!data.contains("FactionID")) {
-                data.putInt("FactionID", 0);
-            }
-            
-            LOGGER.info("§fSyncing data for: " + player.getName().getString() + " | Faction: " + data.getInt("FactionID"));
-        });
-
-        ServerPlayNetworking.registerGlobalReceiver(FACTION_PACKET_ID, (server, player, handler, buf, responseSender) -> {
-            int factionId = buf.readInt();
-            server.execute(() -> {
-                player.getPersistentData().putInt("FactionID", factionId);
-                
-                if (factionId == 1) {
-                    giveMonsterStarterGear(player);
-                } else if (factionId == 2) {
-                    giveGovStarterGear(player);
+    public void onInitializeClient() {
+        // Logic: When the client joins a server, check if they need to see the Entry Screen
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+            // We use a small delay or a check to see if the player has a faction
+            // In 26.1.2, the easiest way is to check the local player's data
+            client.execute(() -> {
+                if (client.player != null) {
+                    // This opens the GUI if they are currently Faction 0 (Neutral)
+                    // Note: PersistentData is server-side, so usually you'd trigger this via a packet
+                    // For now, this is where you'd put the screen trigger:
+                    // Minecraft.getInstance().setScreen(new EntryScreen());
                 }
-
-                LOGGER.info("§a[SUCCESS] §f" + player.getName().getString() + " is now assigned to " + factionId);
             });
         });
     }
-
-    private void giveMonsterStarterGear(ServerPlayer player) {
-        // Placeholder for Monster items
-    }
-
-    private void giveGovStarterGear(ServerPlayer player) {
-        // Placeholder for Government items
-    }
-}v
+}
